7e97397a8a41c421f107b66fc50c97b0a33093dc
joanmontas <joanmontas1@gmail.com>
2024-04-23 21:13:44 -0400

    Accordion

--- src/js/script.js
+++ src/js/script.js
@@ -3,19 +3,20 @@
 // License under GNU General Public License v3.0
 
 $(function() {
-    // Make all unordered lists sortable
     $("ul.sortable").sortable({
         revert: true,
-        connectWith: ".sortable" // Enable dragging between sortable lists
+        connectWith: ".sortable"
     });
 
-    // Make specific list items draggable (optional)
     $(".draggable").draggable({
         connectToSortable: ".sortable",
         helper: "clone",
         revert: "invalid"
     });
 
-    // Prevent text selection while dragging
+    $(".accordion").accordion({
+        collapsible: true
+    });
+
     $("ul, li").disableSelection();
 });
