52f17610e0fe6eeb63e1ea1c40f3b863e17c2fd0
joanmontas <joanmontas1@gmail.com>
2024-04-24 22:07:43 -0400

    Dynamic Additional Button

--- src/js/script.js
+++ src/js/script.js
@@ -8,15 +8,30 @@ $(function() {
         connectWith: ".sortable"
     });
 
-    $(".draggable").draggable({
-        connectToSortable: ".sortable",
-        helper: "clone",
-        revert: "invalid"
-    });
-
     $(".accordion").accordion({
-        collapsible: true
+        collapsible: true,
+        active: false
     });
 
     $("ul, li").disableSelection();
+});
+
+$("#add_todo").on("click", function() {
+    var todoUl = $("#todo_sortable");
+
+    var newLi = $("<li>");
+    var accordionDiv = $("<div>").addClass("accordion");
+    var accordionDivH3 = $("<h3>").text("New Thing");
+    var accordionDivDiv = $("<div>").text("New asd");
+
+    accordionDivH3.appendTo(accordionDiv);
+    accordionDivDiv.appendTo(accordionDiv);
+    accordionDiv.appendTo(newLi);
+    newLi.appendTo(todoUl);
+
+    accordionDiv.accordion({
+        collapsible: true,
+        active: false
+    });
+
 });
